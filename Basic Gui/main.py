95e05443d11e72520eb3ab102ed52a56c1a04b9f
Elder4Ever <112210402+Elder4Ever@users.noreply.github.com>
2023-08-26 20:33:06 -0400

    Add files via upload

--- Basic Gui/main.py
+++ Basic Gui/main.py
@@ -1,12 +1,48 @@
+import os
 import string
+import json
 import random
 from tkinter import *
 #from tkinter.ttk import *
+title = "Brandon's Password Generator"
+version = "Version: Dev-Alpha-1.0"
+lic = "This is a place holder until i get a license. This is a place holder until i get a license. This is a place holder until i get a license. This is a place holder until i get a license. This is a place holder until i get a license. This is a place holder until i get a license. This is a place holder until i get a license. This is a place holder until i get a license. This is a place holder until i get a license. This is a place holder until i get a license. This is a place holder until i get a license. This is a place holder until i get a license."
+def about_screen():
+    def about_on_closing():
+        root.deiconify()
+        about.destroy()
+    root.withdraw()
+    about = Tk()
+    about.resizable(False,False)
+    about.geometry('400x300')
+    about.title("About")
+
+    w = Label(about, text=title,font=("Arial", 18))
+    w.pack()
+
+    ver = Label(about, text=version, font=("Arial", 12))
+    ver.pack()
+
+    license = Label(about, text=lic, font=("Arial", 10), wraplength=350, justify="center")
+    license.pack(padx=20, pady=40)
+    about.protocol("WM_DELETE_WINDOW",about_on_closing)
+    about.mainloop()
+
+def settings():
+    if os.path.exists('C:/Users/Brandon/Desktop/Python/Basic Gui/settings.json'):
+        f = open('C:/Users/Brandon/Desktop/Python/Basic Gui/settings.json','r')
+        data = json.load(f)
+        print(data)
+    else:
+        print('File does not exist')
+
 def print_value(value):
     return value
+
 def on_closing():
      root.clipboard_clear()
      root.destroy()
+
 def securePass():
     password = ""
     passlen = s.get()
@@ -19,11 +55,16 @@ def securePass():
     w.configure(text=password)
     print(password)
     return password
-#passwd = securePass()
+
+
 root = Tk()
 
-root.title("Brandon's Password Generator")
-root.geometry('700x150')
+sw = int(root.winfo_screenwidth() / 2)
+sh = int(root.winfo_screenheight() / 3)
+screen_resolution = str(sw)+'x'+str(sh)
+print(screen_resolution)
+root.title(title)
+root.geometry(screen_resolution)
 #root.resizable(False,False)#
 
 menubar = Menu(root)
@@ -38,7 +79,10 @@ file.add_separator()
 file.add_command(label ='Exit', command = root.destroy)
 edit = Menu(menubar, tearoff = 0)
 menubar.add_cascade(label ='Edit', menu = edit)
-edit.add_command(label ='Settings', command = None)
+edit.add_command(label ='Settings', command = settings)
+help = Menu(menubar, tearoff = 0)
+menubar.add_cascade(label ='Help', menu = help)
+help.add_command(label ='About', command = about_screen)
 root.config(menu = menubar)
 
 
@@ -46,19 +90,15 @@ p = securePass
 
 w = Label(root, text="Click Generate to Start",font=("Arial", 35))
 w.pack()
-s = Scale(root, length=150,width=10, from_=0, to=30, orient=HORIZONTAL,command=print_value)
+
+s = Scale(root, length=200,width=15, from_=0, to=30, orient=HORIZONTAL,command=print_value)
 s.pack()
-frame = Frame(root)
-frame.pack()
 
-bottomframe = Frame(root)
-bottomframe.pack( side = BOTTOM )
+redbutton = Button(root, text='Generate', fg='black', font=("Arial", 20), height = 1, width = 15, command=securePass)
+redbutton.pack()
 
-redbutton = Button(frame, text='Generate', fg='black', font=("Arial", 20), height = 1, width = 15, command=securePass)
-redbutton.pack( side = BOTTOM)
-
-warn = Label(root, text="Automaticlly Copies to Clipboard", fg="red", font=("Arial", 9)).place(x=150, y=120)
-
+warn = Label(root, text="Automaticlly Copies to Clipboard", fg="red", font=("Arial", 9))
+warn.pack( side = BOTTOM)
 
 root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
